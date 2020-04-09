0ed8eca1746e52d222aa47073db14f14c2b3eb4d
Gabriella <gabriellazamudio@yahoo.se>
2020-04-09 14:10:27 +0200

    first homework

---
+++ src/lesson-one.js
@@ -0,0 +1,38 @@
+//Request a length of a side of a square from a user and display the perimeter of the square in the console.//
+
+/*let square = prompt('Specify the side of one side of a square');
+console.log(`The perimeter of the square is:${square*4}`);*/
+
+
+//Request a user’s year of birth, count their age, and display the result to the console. Store the current year as a constant.//
+
+/*let birth = prompt('What year were you born?');
+let year = 2020;
+console.log(`Your are: ${year-birth}`);*/
+
+
+//Request a distance in kilometers between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.//
+
+/*let distance = prompt('How far away is your destination in kilometers?');
+let time = prompt('How much time do you have to get there?');
+console.log(`You have to drive ${distance/time} km/hour to get to your destination on time`);*/
+
+
+//Create a simple currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant. Then display the result in the console.//
+
+/*let usd = prompt('How much USD do you want to convert?');
+const eur = 0.93;
+console.log(`You have ${usd*eur} euros`);*/
+
+
+//A user types in the values of a and b for the formula a * x + b = 0 . The program has to calculate the x and display it in the console.//
+
+/*let a = prompt('Please provide a value for a');
+let b = prompt('Please provide a value for b');
+console.log(`X in the equation a*x+b=0 is: ${-b/a}`);*/
+
+
+//Request current time from a user in format HH:mm (HH is hour in a day from 00 to 23, mm are minutes in hour) and display how many hours and minutes are left until tomorrow.//
+
+let time = prompt('What time is it?');
+console.log(`There is ${} left until a day`);
