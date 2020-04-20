f9b52fe33c529eacff7996fd5b5e2236cfdc4ada
Gabriella <gabriellazamudio@yahoo.se>
2020-04-20 17:46:41 +0200

    third homework

--- src/lesson-three.js
+++ src/lesson-three.js
@@ -10,11 +10,26 @@ function evenOdd(n) {
 console.log(evenOdd(n));*/
 
   //Trolls are attacking your comment section! Create a function that takes a string and returns a new string with all vowels removed. For example, “This website is for losers LOL!” returns “Ths wbst s fr lsrs LL!“.
-
+  /*function removeVowels(string) {
+    let newString = " "; 
+    for (i = 0; i < string.length; i++) {
+        if (string[i] !="a" && string[i] !="e" 
+            && string[i] !="i" && string[i] !="o" && string[i] !="u") {   
+            newString += string[i]; 
+            }
+    }
+    return newString; 
+}
+console.log(removeVowels('Hello World'));*/
 
   //Write a function which repeats the given string exact number of times. For example, repeatString("Hello", 5) returns “HelloHelloHelloHelloHello”.
-function repeatString(Hello) {
-    return 'Hello';
-}
-let greeting = repeatString();
-console.log(greeting);
+  /*function repeat (string, times) {
+      if(times < 0) 
+        return "";
+      if(times === 1) 
+        return string;
+      else 
+        return string + repeat(string, times - 1);
+    }
+    
+console.log(repeat('Hello World!', 5)); */
